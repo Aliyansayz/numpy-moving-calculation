b7e4c4a1242b611a82ea56d078fcea6d785f4bec
Aliyansayz <56589240+Aliyansayz@users.noreply.github.com>
2023-04-13 19:51:44 +0500

    Create simple_moving_average.py

---
+++ simple_moving_average.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def sma (array, period ):
+
+    sma = np.empty_like(array)
+    sma = np.full( sma.shape , np.nan)
+    # Calculate the EMA for each window of 14 values
+    for i in range(period, len(array)+1 ):
+          sma[i-1] = np.mean(array[i-period:i] , dtype=np.float16)
+    return sma 
