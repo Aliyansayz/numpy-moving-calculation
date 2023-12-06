0a2cafb47b00602bc40f74ba34ef64d2735d57cd
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2023-12-07 01:54:41 +0500

    Update simple_moving_average.py
    added SMA version with period N --> value shown in next period N+1 
    
    And Standard deviation from. window members data points subtracted from mean value then squared then summed then divided to give variance and square root of variance gives standard deviation.

--- simple_moving_average.py
+++ simple_moving_average.py
@@ -8,3 +8,26 @@ def sma (array, period ):
     for i in range(period, len(array)+1 ):
           sma[i-1] = np.mean(array[i-period:i] , dtype=np.float16)
     return sma 
+
+def sma (array, period ):
+
+    sma = np.empty_like(array)
+    sma = np.full( sma.shape , np.nan)
+    # Calculate the EMA for each window of 14 values
+    for i in range(period, len(array) ):
+          sma[i] = np.mean(array[i-period:i] , dtype=np.float16)
+    return sma 
+
+def sd (array, sma, period ):
+
+    sd = np.empty_like(array)
+    sd = np.full( sma.shape , np.nan)
+    # Calculate the EMA for each window of 14 values
+    for i in range(period, len(array) ):
+          diff = np.array(array[i-period:i] - sma[i] , dtype=np.float32)
+          sqr_diff = diff ** 2 
+          var = np.sum(sqr_diff) / period 
+          sd[i] = np.sqrt( var ) 
+    return sd
+
+
