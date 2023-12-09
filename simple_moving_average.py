0a5d2338d2254dff9079182a594a023445bce871
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2023-12-09 19:33:07 +0500

    Update simple_moving_average.py

--- simple_moving_average.py
+++ simple_moving_average.py
@@ -2,14 +2,16 @@ import numpy as np
 """
 SMA function in numpy just like pandas one
 """
-def sma(array, window):
-  weights = np.ones(window) / window
+def sma(array, period):
+  weights = np.ones(period) / period
   arr     =  np.convolve(array, weights, mode='valid')
 
+  window = period - 1
   sma = np.empty(window + len(arr), dtype=arr.dtype)
   sma[:window] = np.nan * window
   sma[window:] = arr
-  sma[np.isnan(sma)] = np.nanmean(sma)
+  # fill missing values by taking mean of first values upto chosen period 
+  sma[np.isnan(sma)] = np.nanmean(arr[:period])
   
   return sma
 
